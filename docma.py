3a73f4c22c44374aed24e34c4fff87a53a04cc68
Neil Joshi <nejoshi@cisco.com>
2018-04-23 11:40:19 -0700

    initial commits, does nothing...yet

---
+++ docma.py
@@ -0,0 +1,10 @@
+""" Document and validate schemas in one easy place
+
+    * Licensed under terms of MIT license (see LICENSE-MIT)
+    * Copyright (c) 2018 Neil Joshi
+
+"""
+
+
+__all__ = ['docma']
+__version__ = '0.1.0'
